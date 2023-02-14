1e0733d8177d5034d8041b82876d43a5c8b531b7
linghefor1m <secpro@foxmail.com>
2023-02-14 15:47:23 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: 令和唯一
+ * @Date: 2023-02-14 15:45:50
+ * @LastEditTime: 2023-02-14 15:46:03
+ * @LastEditors: 令和唯一
+ * @Description: 
+ * @FilePath: /nmbxd.js/.eslintrc.js
+ * 关注嘉然,顿顿解馋
+ */
+module.exports = {
+    extends: [],
+    globals: {
+      ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
+      page: true,
+      REACT_APP_ENV: true,
+    },
+  };
+  
